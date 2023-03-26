a827b085c46dec45a5d90fba2b804b508af34ad5
Anthony <aquinoanthony851@gmail.com>
2023-03-25 21:29:33 -0600

    Looping Code Along

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(people = ["Guadalupe", "Ollie", "Aki"], string = "surprise") {
+  const newArray = []
+  for (let i = 0 ; i < people.length; i++) {
+    newArray.push(`Thank you, ${people[i]}, for the wonderful ${string} gift!`);
+  }
+  return newArray;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number--);
+  }
+}
