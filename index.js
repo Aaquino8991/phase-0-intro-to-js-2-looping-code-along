cd29b0b161b8eca87effec603248e039d29fce98
Anthony <aquinoanthony851@gmail.com>
2023-04-16 22:38:48 -0600

    Looping Lab

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 
-function writeCards(people = ["Guadalupe", "Ollie", "Aki"], string = "surprise") {
+function writeCards(people = ['Charlie', 'Samip', 'Ali'], string = "birthday") {
   const newArray = []
   for (let i = 0 ; i < people.length; i++) {
     newArray.push(`Thank you, ${people[i]}, for the wonderful ${string} gift!`);
